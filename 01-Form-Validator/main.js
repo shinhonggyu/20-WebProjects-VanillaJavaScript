e8f35780bfdfd468c9cc24c33d8cf8f26220d6dc
honggyu <zowoz0@naver.com>
2020-08-28 20:19:07 +0900

    Check Length, Email&Password Match

--- 01-Form-Validator/main.js
+++ 01-Form-Validator/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const form = document.querySelector("#form");
 const username = document.querySelector("#username");
 const email = document.querySelector("#email");
@@ -19,9 +21,13 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value)) {
+    showSuccess(input);
+  } else {
+    showError(input, "Eamil in not valid");
+  }
 }
 
 // Cehck required fields
@@ -35,6 +41,27 @@ function checkRequired(inputArr) {
   });
 }
 
+// Check input length
+function checkLength(item, min, max) {
+  if (item.value.length < min) {
+    showError(item, `${getFieldName(item)} must be at least ${min} characters`);
+  } else if (item.value.length > max) {
+    showError(
+      item,
+      `${getFieldName(item)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(item);
+  }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
+
 // Get fieldname
 function getFieldName(item) {
   return item.id.charAt(0).toUpperCase() + item.id.slice(1);
@@ -45,4 +72,8 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });

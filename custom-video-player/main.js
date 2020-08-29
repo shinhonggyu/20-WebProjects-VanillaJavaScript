8e8bbc9866c4131f96c9d168e4c872d4b3d0c9ef
honggyu <zowoz0@naver.com>
2020-08-30 00:38:38 +0900

    03-Custom-Video-player

---
+++ custom-video-player/main.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const video = document.querySelector("#video");
+const play = document.querySelector("#play");
+const stop = document.querySelector("#stop");
+const progress = document.querySelector("#progress");
+const timestamp = document.querySelector("#timestamp");
+
+// Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fas fa-play"></i>';
+  } else {
+    play.innerHTML = '<i class="fas fa-pause"></i>';
+  }
+}
+
+//  Update progress & timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+// Stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+//  Event listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);

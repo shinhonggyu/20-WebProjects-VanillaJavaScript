fb347cb634ea8a9cf4626d78ffa9e17d3ba102e0
honggyu <zowoz0@naver.com>
2020-08-31 22:00:39 +0900

    05-reduce

--- DOM-Array-Methods/main.js
+++ DOM-Array-Methods/main.js
@@ -48,10 +48,22 @@ function sortByRichest() {
   updateDOM();
 }
 
+// Filter only millionaires
 function showMillionaires() {
   data = data.filter((item) => item.money > 1000000);
 
   updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
 }
 
 // Add new obj to data arr
@@ -85,3 +97,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);

82b10eec83846451d596b33dacdae580124476bd
honggyu <zowoz0@naver.com>
2020-08-31 20:48:37 +0900

    05-sort

--- DOM-Array-Methods/main.js
+++ DOM-Array-Methods/main.js
@@ -41,6 +41,13 @@ function doubleMoney() {
   updateDOM();
 }
 
+// Sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+
+  updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -70,3 +77,4 @@ function formatMoney(number) {
 
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);

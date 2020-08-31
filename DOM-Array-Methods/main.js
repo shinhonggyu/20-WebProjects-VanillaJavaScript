cfdd7689e52757a46e21225f7545ca5ea44b150f
honggyu <zowoz0@naver.com>
2020-08-31 19:34:35 +0900

    05-forEach

--- DOM-Array-Methods/main.js
+++ DOM-Array-Methods/main.js
@@ -35,4 +35,28 @@ async function getRandomUser() {
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+// Update DOM
+function updateDOM(provideDate = data) {
+  // Clear main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  provideDate.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+addUserBtn.addEventListener("click", getRandomUser);

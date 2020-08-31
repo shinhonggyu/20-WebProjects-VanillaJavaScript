fd9366770c377fabd23294d0cef9b0ed4f4805f7
honggyu <zowoz0@naver.com>
2020-08-31 16:25:41 +0900

    05-Fetch/Async/Await

---
+++ DOM-Array-Methods/main.js
@@ -0,0 +1,38 @@
+const main = document.querySelector("#main");
+const addUserBtn = document.querySelector("#add-user");
+const doubleBtn = document.querySelector("#double");
+const showMillionairesBtn = document.querySelector("#show-millionaires");
+const sortBtn = document.querySelector("#sort");
+const calculateWealthBtn = document.querySelector("#calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+// function getRandomUser() {
+//   fetch("https://randomuser.me/api")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// }
+
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+}

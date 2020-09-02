603a0678f8f158d200696c4c9a5e571ebcf35f66
honggyu <zowoz0@naver.com>
2020-09-02 17:14:26 +0900

    07-Display Words Function

---
+++ hangman/main.js
@@ -0,0 +1,39 @@
+const wordEl = document.querySelector("#word");
+const wrongLettersEl = document.querySelector("#wrong-letters");
+const playAgainBtn = document.querySelector("#play-again");
+const popUp = document.querySelector("#popup-container");
+const notification = document.querySelector("#notification-container");
+const finalMessage = document.querySelector("#final-message");
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+        ${selectedWord
+          .split("")
+          .map(
+            (letter) =>
+              `<span class="letter">${
+                correctLetters.includes(letter) ? letter : ""
+              }</span>`
+          )
+          .join("")}
+    `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations! You Won! 😁";
+    popUp.style.display = "flex";
+  }
+}
+
+displayWord();

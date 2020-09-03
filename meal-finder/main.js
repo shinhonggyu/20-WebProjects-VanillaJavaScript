3ea0ac957d082169e9e9dfb36b16f57870cc54bf
honggyu <zowoz0@naver.com>
2020-09-03 17:55:59 +0900

    Search & Display Meals From API

--- meal-finder/main.js
+++ meal-finder/main.js
@@ -1,1 +1,53 @@
 "use strict";
+const search = document.querySelector("#search");
+const submit = document.querySelector("#submit");
+const random = document.querySelector("#random");
+const resultHeading = document.querySelector("#result-heading");
+const mealsEl = document.querySelector("#meals");
+const single_mealEl = document.querySelector("#single-meal");
+
+// Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = "";
+
+  // Get search term
+  const term = search.value;
+
+  // Check for empty
+  if (term) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>search results for '${term}'</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+                <div class="meal">
+                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                    <div class="meal-info" data-mealID="${meal.idMeal}">
+                        <h3>${meal.strMeal}</h3>
+                    </div>
+                </div>
+            `
+            )
+            .join("");
+        }
+      });
+    // Clear search text
+    search.value = "";
+  } else {
+    search.focus();
+    return;
+  }
+}
+
+// Evnet listeners
+submit.addEventListener("submit", searchMeal);

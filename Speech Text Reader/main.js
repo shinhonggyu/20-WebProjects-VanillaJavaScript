357f1745420a3d82dc27ae28e2093f42b6196695
honggyu <zowoz0@naver.com>
2020-09-07 21:47:39 +0900

    13-HTML & Output Speech Boxes

---
+++ Speech Text Reader/main.js
@@ -0,0 +1,78 @@
+" use strict";
+
+const main = document.querySelector("main");
+const voicesSelect = document.querySelector("#voices");
+const textarea = document.querySelector("#text");
+const readBtn = document.querySelector("#read");
+const toggleBtn = document.querySelector("#toggle");
+const closeBtn = document.querySelector("#close");
+
+const data = [
+  {
+    image: "./img/drink.jpg",
+    text: "I'm Thirsty",
+  },
+  {
+    image: "./img/food.jpg",
+    text: "I'm Hungry",
+  },
+  {
+    image: "./img/tired.jpg",
+    text: "I'm Tired",
+  },
+  {
+    image: "./img/hurt.jpg",
+    text: "I'm Hurt",
+  },
+  {
+    image: "./img/happy.jpg",
+    text: "I'm Happy",
+  },
+  {
+    image: "./img/angry.jpg",
+    text: "I'm Angry",
+  },
+  {
+    image: "./img/sad.jpg",
+    text: "I'm Sad",
+  },
+  {
+    image: "./img/scared.jpg",
+    text: "I'm Scared",
+  },
+  {
+    image: "./img/outside.jpg",
+    text: "I Want To Go Outside",
+  },
+  {
+    image: "./img/home.jpg",
+    text: "I Want To Go Home",
+  },
+  {
+    image: "./img/school.jpg",
+    text: "I Want To Go To School",
+  },
+  {
+    image: "./img/grandma.jpg",
+    text: "I Want To Go To Grandmas",
+  },
+];
+
+data.forEach(createBox);
+
+// Create speech boxes
+function createBox(item) {
+  const box = document.createElement("div");
+
+  const { image, text } = item;
+
+  box.classList.add("box");
+  box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+
+  // @todo - speak event
+
+  main.appendChild(box);
+}
